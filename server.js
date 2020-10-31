b3d3762606bbc7a9a97e34f461676904aa258850
Selina Hussain <61423748+selinahussain@users.noreply.github.com>
2020-10-31 16:51:40 +0000

    routes to stratford pharmacies in level two made

--- server.js
+++ server.js
@@ -5,15 +5,30 @@ const stratfordData = require("./data/Stratford.json");
 const heathrowData = require("./data/Heathrow.json");
 const harrowData = require("./data/Harrow.json");
 
-//stratford pharmacies
 
 app.get("/", (req, res) => {
   res.send("try /pharmacies");
 })
 
+//stratford pharmacies
+
 app.get("/pharmacies", (req, res) => {
   res.json(stratfordData.pharmacies);
 })
+
+app,get("/colleges", (req, res) => {
+  res.json(stratfordData.colleges);
+})
+
+app,get("/doctors", (req, res) => {
+  res.json(stratfordData.doctors);
+})
+
+app,get("/hospitals", (req, res) => {
+  res.json(stratfordData.hospitals);
+})
+
+
 
 const listener = app.listen(process.env.PORT, function() {
   console.log("Your app is listening on port " + listener.address().port);
